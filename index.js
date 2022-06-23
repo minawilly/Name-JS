34a398554b4e96a9a3878ac36cf92e3f160f9200
minawilly <aminahwilliamsade@gmail.com>
2022-06-23 01:18:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var myname ="Aminah";
+var height = "1.62m"
+var country = "Nigeria"
+
+alert("Hello! I'm" + " " + myname + ", " + "I'm" + " " + height + " " + "tall" + " " + "and" + " " + "I'm from" + " " + country + ".");
